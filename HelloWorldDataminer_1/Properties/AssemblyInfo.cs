cc0a6979a177ef8d1b06a64671974c1f3296f4de
Oliver Wilkinson <Oliver.wilkinson@ericsson.com>
2023-03-28 13:26:48 +0100

    Add project files.

---
+++ HelloWorldDataminer_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldDataminer_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWorldDataminer_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("EDF8A12A-0FD5-4E09-B83F-B081BE2BCCD8")]
+[assembly: AssemblyVersion("1.0.0.0")]
